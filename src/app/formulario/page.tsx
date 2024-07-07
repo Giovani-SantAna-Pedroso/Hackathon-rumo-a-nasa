fcfc669d4ca37edd93d4b02b1c749f5b637da129
Giovani Sant'Ana Ferreira Pedroso Vaz <giovanisfpv@gmail.com>
2024-07-07 17:14:54 -0300

    minor fixies

--- src/app/formulario/page.tsx
+++ src/app/formulario/page.tsx
@@ -6,122 +6,13 @@ import img2 from '@public/images/personagem/2.png';
 import img3 from '@public/images/personagem/3.png';
 import Image from 'next/image';
 import { KoHo } from 'next/font/google';
+import { useRouter } from 'next/navigation';
+import { perguntasTest } from '@/utils/variables';
 const koho = KoHo({ subsets: ['latin'], weight: '600' });
 const koho2 = KoHo({ subsets: ['latin'], weight: '600' });
 
 function Dashboard() {
-  const perguntas = [
-    {
-      section: 'Informações Básicas',
-      titulo: 'Nome Completo',
-      question:
-        'Precisamos saber quem você é para poder oferecer uma ajuda personalizada',
-    },
-    {
-      section: 'Informações Básicas',
-      titulo: 'Nome da Empresa',
-      question:
-        'Saber o nome da sua empresa nos ajuda a identificar seu histórico e entender melhor seu contexto',
-    },
-    {
-      section: 'Informações Básicas',
-      titulo: 'Setor de Atuação',
-      question:
-        'Em que setor sua empresa operava? Isso nos ajuda a entender os desafios específicos que você enfrenta',
-    },
-    {
-      section: 'Informações Básicas',
-      titulo: 'Localização da Empresa',
-      question:
-        'Em que setor sua empresa operava? Isso nos ajuda a entender os desafios específicos que você enfrenta',
-    },
-    // {
-    //   section: 'Impacto do Desastre',
-    //   titulo: 'Descreva o impacto do desastre na sua empresa:',
-    //   question:
-    //     'Gostaríamos de saber como o desastre afetou sua empresa para entender a gravidade da situação e como podemos ajudar.',
-    // },
-    // {
-    //   section: 'Impacto do Desastre',
-    //   titulo: 'Quando o desastre ocorreu?',
-    //   question:
-    //     'Saber a data do evento nos ajuda a avaliar a urgência do apoio necessário.',
-    // },
-    // {
-    //   section: 'Impacto do Desastre',
-    //   titulo: 'Quais foram as principais perdas que você enfrentou?',
-    //   question:
-    //     'Nos conte sobre as principais perdas, como danos ao prédio ou perda de equipamentos, para podermos direcionar melhor a ajuda.',
-    // },
-    // {
-    //   section: 'Situação Financeira da Empresa',
-    //   titulo:
-    //     'Qual era o faturamento médio mensal da sua empresa nos últimos 12 meses antes do desastre?',
-    //   question:
-    //     'Essa informação nos ajuda a entender a saúde financeira da sua empresa antes do desastre.',
-    // },
-    // {
-    //   section: 'Situação Financeira da Empresa',
-    //   titulo:
-    //     'Você tinha algum empréstimo ou financiamento ativo antes do desastre? Se sim, qual era o valor pendente?',
-    //   question:
-    //     'Saber sobre financiamentos anteriores nos ajuda a planejar a melhor forma de reestruturar suas finanças.',
-    // },
-    // {
-    //   section: 'Situação Financeira da Empresa',
-    //   titulo:
-    //     'Qual era a margem de lucro da sua empresa (em percentual) nos últimos 12 meses antes do desastre?',
-    //   question:
-    //     'Entender a rentabilidade da sua empresa nos dá uma ideia da sua capacidade de recuperação.',
-    // },
-    // {
-    //   section: 'Situação Financeira da Empresa',
-    //   titulo: 'Sua empresa tinha seguro contra desastres?',
-    //   question:
-    //     'Queremos saber se há algum recurso adicional disponível para ajudar na recuperação.',
-    // },
-    // {
-    //   section: 'Situação Financeira da Empresa',
-    //   titulo: 'Se sim, qual é o valor que você espera receber do seguro?',
-    //   question:
-    //     'Isso nos ajuda a calcular a quantia adicional necessária para sua recuperação.',
-    // },
-    // {
-    //   section: 'Necessidades e Planejamento',
-    //   titulo:
-    //     'Quanto crédito você acredita que seria necessário para reerguer sua empresa?',
-    //   question:
-    //     'Queremos saber quanto você estima ser necessário para que possamos avaliar a viabilidade do crédito.',
-    // },
-    // {
-    //   section: 'Necessidades e Planejamento',
-    //   titulo:
-    //     'Quais seriam as principais áreas de investimento com esse crédito?',
-    //   question:
-    //     'Por favor, nos conte como você planeja usar o crédito. Isso nos ajuda a entender suas prioridades e necessidades específicas.',
-    // },
-    // {
-    //   section: 'Necessidades e Planejamento',
-    //   titulo:
-    //     'Você tem algum plano de contingência ou estratégia de recuperação? Se sim, por favor, descreva brevemente.',
-    //   question:
-    //     'Ter um plano mostra que você está preparado para a recuperação. Compartilhe conosco para que possamos alinhar nosso apoio.',
-    // },
-    // {
-    //   section: 'Suporte Adicional',
-    //   titulo:
-    //     'Você gostaria de receber consultoria financeira ou de negócios para ajudar na recuperação da sua empresa?',
-    //   question:
-    //     'Estamos aqui para ajudar além do crédito, oferecendo suporte e orientação.',
-    // },
-    // {
-    //   section: 'Suporte Adicional',
-    //   titulo:
-    //     'Há mais alguma informação que você gostaria de compartilhar conosco para melhor entendermos sua situação?',
-    //   question:
-    //     'Este espaço é para você compartilhar qualquer detalhe adicional que possa ser relevante para entendermos completamente sua situação.',
-    // },
-  ];
+  const perguntas = perguntasTest;
   const imgs = [
     img0,
     img1,
@@ -146,6 +37,7 @@ function Dashboard() {
   ];
   const [step, setStep] = React.useState(0);
   const [ans, setAns] = React.useState(Array(perguntas.length).fill(''));
+  const router = useRouter();
   const handleInput = (e: any) => {
     const cpAns = [...ans];
     cpAns[step] = e.target.value;
@@ -161,6 +53,7 @@ function Dashboard() {
         console.log(indexOfEmpty);
         return;
       }
+      router.push('CreditosDisponiveis');
       return;
     }
     setStep((atual) => atual + 1);
--- src/utils/variables.tsx
+++ src/utils/variables.tsx
@@ -1,7 +1,144 @@
 export const buttonText = 'Call to Action';
 // export const  linkToPayment ="/agendar-consulta-rapida"
 export const linkToPayment = '#';
-
+export const perguntas = [
+  {
+    section: 'Informações Básicas',
+    titulo: 'Nome Completo',
+    question:
+      'Precisamos saber quem você é para poder oferecer uma ajuda personalizada',
+  },
+  {
+    section: 'Informações Básicas',
+    titulo: 'Nome da Empresa',
+    question:
+      'Saber o nome da sua empresa nos ajuda a identificar seu histórico e entender melhor seu contexto',
+  },
+  {
+    section: 'Informações Básicas',
+    titulo: 'Setor de Atuação',
+    question:
+      'Em que setor sua empresa operava? Isso nos ajuda a entender os desafios específicos que você enfrenta',
+  },
+  {
+    section: 'Informações Básicas',
+    titulo: 'Localização da Empresa',
+    question:
+      'Em que setor sua empresa operava? Isso nos ajuda a entender os desafios específicos que você enfrenta',
+  },
+  {
+    section: 'Impacto do Desastre',
+    titulo: 'Descreva o impacto do desastre na sua empresa:',
+    question:
+      'Gostaríamos de saber como o desastre afetou sua empresa para entender a gravidade da situação e como podemos ajudar.',
+  },
+  {
+    section: 'Impacto do Desastre',
+    titulo: 'Quando o desastre ocorreu?',
+    question:
+      'Saber a data do evento nos ajuda a avaliar a urgência do apoio necessário.',
+  },
+  {
+    section: 'Impacto do Desastre',
+    titulo: 'Quais foram as principais perdas que você enfrentou?',
+    question:
+      'Nos conte sobre as principais perdas, como danos ao prédio ou perda de equipamentos, para podermos direcionar melhor a ajuda.',
+  },
+  {
+    section: 'Situação Financeira da Empresa',
+    titulo:
+      'Qual era o faturamento médio mensal da sua empresa nos últimos 12 meses antes do desastre?',
+    question:
+      'Essa informação nos ajuda a entender a saúde financeira da sua empresa antes do desastre.',
+  },
+  {
+    section: 'Situação Financeira da Empresa',
+    titulo:
+      'Você tinha algum empréstimo ou financiamento ativo antes do desastre? Se sim, qual era o valor pendente?',
+    question:
+      'Saber sobre financiamentos anteriores nos ajuda a planejar a melhor forma de reestruturar suas finanças.',
+  },
+  {
+    section: 'Situação Financeira da Empresa',
+    titulo:
+      'Qual era a margem de lucro da sua empresa (em percentual) nos últimos 12 meses antes do desastre?',
+    question:
+      'Entender a rentabilidade da sua empresa nos dá uma ideia da sua capacidade de recuperação.',
+  },
+  {
+    section: 'Situação Financeira da Empresa',
+    titulo: 'Sua empresa tinha seguro contra desastres?',
+    question:
+      'Queremos saber se há algum recurso adicional disponível para ajudar na recuperação.',
+  },
+  {
+    section: 'Situação Financeira da Empresa',
+    titulo: 'Se sim, qual é o valor que você espera receber do seguro?',
+    question:
+      'Isso nos ajuda a calcular a quantia adicional necessária para sua recuperação.',
+  },
+  {
+    section: 'Necessidades e Planejamento',
+    titulo:
+      'Quanto crédito você acredita que seria necessário para reerguer sua empresa?',
+    question:
+      'Queremos saber quanto você estima ser necessário para que possamos avaliar a viabilidade do crédito.',
+  },
+  {
+    section: 'Necessidades e Planejamento',
+    titulo:
+      'Quais seriam as principais áreas de investimento com esse crédito?',
+    question:
+      'Por favor, nos conte como você planeja usar o crédito. Isso nos ajuda a entender suas prioridades e necessidades específicas.',
+  },
+  {
+    section: 'Necessidades e Planejamento',
+    titulo:
+      'Você tem algum plano de contingência ou estratégia de recuperação? Se sim, por favor, descreva brevemente.',
+    question:
+      'Ter um plano mostra que você está preparado para a recuperação. Compartilhe conosco para que possamos alinhar nosso apoio.',
+  },
+  {
+    section: 'Suporte Adicional',
+    titulo:
+      'Você gostaria de receber consultoria financeira ou de negócios para ajudar na recuperação da sua empresa?',
+    question:
+      'Estamos aqui para ajudar além do crédito, oferecendo suporte e orientação.',
+  },
+  {
+    section: 'Suporte Adicional',
+    titulo:
+      'Há mais alguma informação que você gostaria de compartilhar conosco para melhor entendermos sua situação?',
+    question:
+      'Este espaço é para você compartilhar qualquer detalhe adicional que possa ser relevante para entendermos completamente sua situação.',
+  },
+];
+export const perguntasTest = [
+  {
+    section: 'Informações Básicas',
+    titulo: 'Nome Completo',
+    question:
+      'Precisamos saber quem você é para poder oferecer uma ajuda personalizada',
+  },
+  {
+    section: 'Informações Básicas',
+    titulo: 'Nome da Empresa',
+    question:
+      'Saber o nome da sua empresa nos ajuda a identificar seu histórico e entender melhor seu contexto',
+  },
+  {
+    section: 'Informações Básicas',
+    titulo: 'Setor de Atuação',
+    question:
+      'Em que setor sua empresa operava? Isso nos ajuda a entender os desafios específicos que você enfrenta',
+  },
+  {
+    section: 'Informações Básicas',
+    titulo: 'Localização da Empresa',
+    question:
+      'Em que setor sua empresa operava? Isso nos ajuda a entender os desafios específicos que você enfrenta',
+  },
+];
 export const plansAvalible = [
   {
     name: 'Consulta individual',
