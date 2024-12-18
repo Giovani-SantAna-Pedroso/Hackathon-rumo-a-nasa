197456cf99232fa7bf54a007e9cbd721679d1918
rafaelrvs <rafaelvieira1333@gmail.com>
2024-12-18 00:28:36 -0300

    adicionando dashboard

--- src/app/CreditosDisponiveis/page.jsx
+++ src/app/CreditosDisponiveis/page.jsx
@@ -4,30 +4,10 @@ import { useRouter } from 'next/navigation';
 import './CreditosDisponiveis.css';
 
 const proposals = [
-  {
-    id: '#1',
-    rate: '5%',
-    description: 'construção + créditos',
-    payment: 'P /pagamento até 90 dias',
-  },
-  {
-    id: '#2',
-    rate: '6%',
-    description: 'construção + créditos',
-    payment: 'P /pagamento até 90 dias',
-  },
-  {
-    id: '#3',
-    rate: '6.1%',
-    description: 'construção + créditos',
-    payment: 'P /pagamento até 90 dias',
-  },
-  {
-    id: '#4',
-    rate: '7%',
-    description: 'construção + créditos',
-    payment: 'P /pagamento até 90 dias',
-  },
+  { id: '#1', rate: '5%', description: 'Construção + Créditos', payment: 'Pagamento até 90 dias' },
+  { id: '#2', rate: '6%', description: 'Construção + Créditos', payment: 'Pagamento até 90 dias' },
+  { id: '#3', rate: '6.1%', description: 'Construção + Créditos', payment: 'Pagamento até 90 dias' },
+  { id: '#4', rate: '7%', description: 'Construção + Créditos', payment: 'Pagamento até 90 dias' },
 ];
 
 function CreditosDisponiveis() {
@@ -37,7 +17,6 @@ function CreditosDisponiveis() {
   const handlerPerfil = async () => {
     setIsLoading(true);
     try {
-      // Simular chamada à API de login
       await new Promise((resolve) => setTimeout(resolve, 3000));
       router.push('meuPerfil');
     } catch (error) {
@@ -51,33 +30,32 @@ function CreditosDisponiveis() {
     <main className="conteudo-principal">
       <div className="content">
         <header className="content-header">
-          Melhor proposta para você{' '}
-          <p onClick={handlerPerfil} className="meuPerfil">
-            {isLoading ? 'loading...' : 'Meu perfil'}
-          </p>
+          <h1>Melhor proposta para você</h1>
+          <button onClick={handlerPerfil} className="meuPerfil" disabled={isLoading}>
+            {isLoading ? (
+              <div className="spinner">
+                <div className="double-bounce1"></div>
+                <div className="double-bounce2"></div>
+              </div>
+            ) : (
+              'Meu Perfil'
+            )}
+          </button>
         </header>
 
         <div className="box-content">
           <div className="title-box-content">
-            <p id="qtd">QTD</p>
-            <p>TAXA</p>
-            <p>DESCRIÇÃO</p>
-            <p>FORMA DE PAGAMENTO</p>
+            <p>ID</p>
+            <p>Taxa</p>
+            <p>Descrição</p>
+            <p>Forma de Pagamento</p>
           </div>
           {proposals.map((item) => (
-            <div key={item.id}>
-              <a href=" https://www.itau.com.br/apoio-ao-sul" target="_blank">
-                <div className="proposal-item">
-                  <div className="proposal-id">ID: {item.id}</div>
-                  <div className="proposal-rate">Taxa: {item.rate}</div>
-                  <div className="proposal-description">
-                    Descrição: {item.description}
-                  </div>
-                  <div className="proposal-payment">
-                    Pagamento: {item.payment}
-                  </div>
-                </div>
-              </a>
+            <div key={item.id} className="proposal-item">
+              <div className="proposal-id">{item.id}</div>
+              <div className="proposal-rate">{item.rate}</div>
+              <div className="proposal-description">{item.description}</div>
+              <div className="proposal-payment">{item.payment}</div>
             </div>
           ))}
         </div>
